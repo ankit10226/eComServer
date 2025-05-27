f28574f8fbc7804b8a4ba09a9ef0235ad0e10cb6
Ankit Singh <ankit10226.as@gmail.com>
2025-05-27 21:13:09 +0530

    server

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user-register', function () {
-    // dd('hit');
-    return response()->json(["status"=>true,"message"=>"test running"]);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::prefix('v1')->group(function () {
+    Route::post('/test', function (Request $request) {
+        return response()->json(['status' => 1, 'data' => $request->all()]);
+    });
 });
